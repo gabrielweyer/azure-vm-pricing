cbd82c09339c98fb6690579a8c3d205da46a9e6a
Gabriel Weyer <gabrielweyer@gmail.com>
2022-03-14 16:40:43 +1100

    Remove empty line after Arrange / Act / Assert

--- coster/tests/AzureVmCosterTests/Models/FileIdentifierTests.cs
+++ coster/tests/AzureVmCosterTests/Models/FileIdentifierTests.cs
@@ -6,15 +6,12 @@ public static class FileIdentifierTests
     public static void GivenInitialisedIdentifier_WhenGetPricingFilename_ThenExpected()
     {
         // Arrange
-
         var identifier = new FileIdentifier("some-region", "some-operating-system");
 
         // Actual
-
         var actual = identifier.GetPricingFilename();
 
         // Assert
-
         actual.Should().Be("vm-pricing_some-region_some-operating-system.json");
     }
 
@@ -22,15 +19,12 @@ public static class FileIdentifierTests
     public static void GivenValidFilename_WhenFrom_Then()
     {
         // Arrange
-
         var file = new FileInfo(@"E:\tmp\vm-pricing_some-region_some-operating-system.json");
 
         // Act
-
         var actual = FileIdentifier.From(file);
 
         // Assert
-
         var expected = new FileIdentifier("some-region", "some-operating-system");
 
         actual.Should().BeEquivalentTo(expected);
