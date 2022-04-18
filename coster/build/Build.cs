f239194b3e90bd8091197d01176ab86dd7ebfc2f
Gabriel Weyer <gabrielweyer@gmail.com>
2022-04-18 10:44:51 +1000

    Publish coster coverage as HTML

--- coster/build/Build.cs
+++ coster/build/Build.cs
@@ -6,6 +6,7 @@ using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.ReportGenerator;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
@@ -20,7 +21,7 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main() => Execute<Build>(x => x.Test);
+    public static int Main() => Execute<Build>(x => x.GenerateCoverage);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
@@ -31,6 +32,7 @@ class Build : NukeBuild
     static AbsolutePath TestsDirectory => RootDirectory / "tests";
     static AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
     static AbsolutePath TestResultsDirectory => ArtifactsDirectory / "test-results";
+    static AbsolutePath CodeCoverageDirectory => ArtifactsDirectory / "coverage-report";
 
 #pragma warning disable CA1822 // Can't make this static as it breaks NUKE
     Target Clean => _ => _
@@ -79,6 +81,7 @@ class Build : NukeBuild
 
             DotNetTest(s => s
                 .SetConfiguration(Configuration)
+                .SetDataCollector("XPlat Code Coverage")
                 .EnableNoBuild()
                 .CombineWith(testProjects, (ss, p) =>
                 {
@@ -91,5 +94,16 @@ class Build : NukeBuild
                         .SetResultsDirectory(testResultsDirectory)
                         .SetLoggers($"html;LogFileName={testResultsName}.html");
                 }), completeOnFailure: true);
+        });
+
+    Target GenerateCoverage => _ => _
+        .DependsOn(Test)
+        .Executes(() =>
+        {
+            ReportGeneratorTasks.ReportGenerator(s => s
+                .SetFramework("net6.0")
+                .SetReports($"{TestResultsDirectory}/**/coverage.cobertura.xml")
+                .SetTargetDirectory(CodeCoverageDirectory)
+                .SetReportTypes(ReportTypes.Html));
         });
 }
