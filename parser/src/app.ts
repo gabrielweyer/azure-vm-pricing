d6f25e3275bc9fb985f505b043c13ce2e2908d51
Gabriel Weyer <gabrielweyer@gmail.com>
2022-07-17 08:54:00 +1000

    Block survey and ads networks

--- parser/src/app.ts
+++ parser/src/app.ts
@@ -45,6 +45,54 @@ function timeEvent(eventName: string): void {
   previousPerformanceNow = happenedAt;
 }
 
+const blockedUrls = [
+  'https://www.microsoft.com/library/svy/azure/broker.js',
+];
+
+const blockedDomains = [
+  'https://munchkin.marketo.net/',
+  'https://cdnssl.clicktale.net/',
+  'https://publisher.liveperson.net/',
+  'https://www.facebook.com/',
+  'https://googleads.g.doubleclick.net/',
+  'https://dc.ads.linkedin.com/',
+  'https://dpm.demdex.net/',
+  'https://mscom.demdex.net/',
+  'https://msftenterprise.sc.omtrdc.net/',
+  'https://cm.everesttech.net/',
+  'https://sync.mathtag.com/',
+  'https://ib.adnxs.com/',
+  'https://idsync.rlcdn.com/',
+  'https://cm.g.doubleclick.net/',
+  'https://rtd.tubemogul.com/',
+  'https://idpix.media6degrees.com/',
+  'https://ad.doubleclick.net/'
+];
+
+const blockedFiles = [
+  '/LivePersonChat-iframe.js'
+];
+
+function isBlocked(url: string): boolean {
+  if (blockedUrls.includes(url)) {
+    return true;
+  }
+
+  for (let offset = 0; offset < blockedDomains.length; offset++) {
+    if (url.startsWith(blockedDomains[offset])) {
+      return true;
+    }
+  }
+
+  for (let offset = 0; offset < blockedFiles.length; offset++) {
+    if (url.endsWith(blockedFiles[offset])) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 (async function() {
   recordTiming = false;
   const runHeadless = true;
@@ -100,10 +148,22 @@ function timeEvent(eventName: string): void {
 
   try
   {
-    timeEvent('pageStartedAt');
+    timeEvent('pageCreateStartedAt');
     const page = await browser.newPage();
     page.setDefaultNavigationTimeout(60000);
-    timeEvent('pageStartedAt');
+    page.setRequestInterception(true);
+    timeEvent('pageCreateCompletedAt');
+
+    page.on('request', req => {
+      const url = req.url();
+
+      if (isBlocked(url)) {
+        req.abort();
+        return;
+      }
+
+      req.continue();
+    });
 
     page.on('console', (log) => {
       if (log.type() === 'warning' || log.type() === 'error') {
