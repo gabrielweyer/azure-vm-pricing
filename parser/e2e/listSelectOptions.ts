4dd3f495a2c043a890a37848c620e544c453ebfd
Gabriel Weyer <gabrielweyer@gmail.com>
2024-10-24 07:49:10 +1100

    List available currency, OS/Software, and region

---
+++ parser/e2e/listSelectOptions.ts
@@ -0,0 +1,26 @@
+import puppeteer from "puppeteer";
+
+export default async function listSelectOptions(selectName: string): Promise<string[]> {
+  const browser = await puppeteer.launch({headless: true});
+  try
+  {
+    const page = await browser.newPage();
+    await page.goto(`https://azure.microsoft.com/en-us/pricing/details/virtual-machines/windows/`);
+
+    await page.waitForFunction(
+      async () => {
+        const applicableVmsAnnouncemnt = <HTMLSpanElement> document.querySelector('#pricing-announcement');
+        return applicableVmsAnnouncemnt !== null;
+      }
+    );
+
+    const selector = `select[name=${selectName}]`;
+    return await page.$eval(selector, select => Array.from((<HTMLSelectElement> select).options).map(c => `${c.value} (${c.text})`));
+  }
+  finally
+  {
+    if (browser) {
+      await browser.close();
+    }
+  }
+}
